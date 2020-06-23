879c69f0fd5ade9f4f5d39766b087891e2114ea8
Chris Weiss <bitblit@gmail.com>
2020-06-23 00:58:38 -0700

    Initial build

---
+++ README.md
@@ -0,0 +1,10 @@
+# AngularElectron
+
+# Tags
+* Use the "angular8electron9" tag to get a basic app with angular 8 and electron 9 
+
+
+# References
+
+* This originally came from [tutorial on setting up Electron Webpack with Angular]
+    (https://medium.com/@darren.kendraio/using-angular-with-electron-webpack-b9763903823c), at least sorta.
---
+++ src/renderer/index.ts
@@ -0,0 +1,32 @@
+// Polyfills
+import "core-js/es7/reflect";
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+// require("zone.js/dist/zone");
+
+// Vendor
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+import "@angular/core";
+import "@angular/common";
+import "@angular/router";
+import "rxjs";
+
+// main
+
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { enableProdMode } from "@angular/core";
+import { AppModule } from "./app/app.module";
+import {No} from '@bitblit/ratchet/dist/common/no';
+
+if(process.env.NODE_ENV === "production") {
+    enableProdMode();
+}
+
+// add base element for Angular Router support
+const base: HTMLBaseElement = document.createElement('base');
+// this is './' not '/' because electron uses file:// URLs
+base.href = './';
+document.head.appendChild(base);
+
+platformBrowserDynamic().bootstrapModule(AppModule).then(No.op);
